885e5059eceab82f92c35e8968e8482e12c74510
JpPrates21 <jpprates2016@gmail.com>
2024-10-17 08:42:11 -0300

    Avaliação de Melhor Sistema

--- sistema.c
+++ sistema.c
@@ -55,3 +55,81 @@ void LeSistemas(NumSistemas* s, int n_sistemas) { //Faz a leitura do quantidade 
         LePlanetas(&(s->SistemasSistemas[i]), s->SistemasSistemas[i].QuantPlanetas);
     }
 }
+
+int AvaliaMelhorSistema(const SistemaSolar* sistemaA, const SistemaSolar* sistemaB) { // Faz toda a comparação entre sistemas para descobrir qual o mais interessante
+    if (sistemaA->RaioSistema > sistemaB->RaioSistema) {// Maior Raio
+        return 1;
+    } else if (sistemaA->RaioSistema < sistemaB->RaioSistema) {
+        return 0;
+    }
+
+    if (sistemaA->QuantPlanetas > sistemaB->QuantPlanetas) {// Maior quantidade de planetas
+        return 1;
+    } else if (sistemaA->QuantPlanetas < sistemaB->QuantPlanetas) {
+        return 0;
+    }
+
+    if (sistemaA->QuantPlanetas > 0) { //Qual sistema possui o maior planeta
+        int maiorRaioPlanetaA = sistemaA->PlanetasSistema[0].RaioPlaneta;
+        int maiorRaioPlanetaB = sistemaB->PlanetasSistema[0].RaioPlaneta;
+
+        for (int i = 1; i < sistemaA->QuantPlanetas; i++) {
+            if (sistemaA->PlanetasSistema[i].RaioPlaneta > maiorRaioPlanetaA) {//Passa por todos os planetas do sistema A
+                maiorRaioPlanetaA = sistemaA->PlanetasSistema[i].RaioPlaneta;
+            }
+        }
+
+        for (int i = 1; i < sistemaB->QuantPlanetas; i++) {
+            if (sistemaB->PlanetasSistema[i].RaioPlaneta > maiorRaioPlanetaB) {//Passsa por todos os planetas do sistema B
+                maiorRaioPlanetaB = sistemaB->PlanetasSistema[i].RaioPlaneta;
+            }
+        }
+
+        //Pega o maior planeta do sistema A e compara com o maior planeta do sistema B e retorna o resultado
+        if (maiorRaioPlanetaA > maiorRaioPlanetaB) {
+            return 1;
+        } else if (maiorRaioPlanetaA < maiorRaioPlanetaB) {
+            return 0;
+        }
+    }
+
+    if (sistemaA->PlanetasSistema[0].QuantLuas > sistemaB->PlanetasSistema[0].QuantLuas) {//Qual sistema possui a maior quantidade de luas
+        return 1;
+    } else if (sistemaA->PlanetasSistema[0].QuantLuas < sistemaB->PlanetasSistema[0].QuantLuas) {
+        return 0;
+    }
+
+    if (sistemaA->PlanetasSistema[0].QuantLuas > 0) {//Qual planeta possui a maior lua
+        int maiorRaioLuaA = sistemaA->PlanetasSistema[0].Luas[0].RaioLua;
+        int maiorRaioLuaB = sistemaB->PlanetasSistema[0].Luas[0].RaioLua;
+
+        for (int i = 0; i < sistemaA->QuantPlanetas; i++) {
+            for (int j = 0; j < sistemaA->PlanetasSistema[i].QuantLuas; j++) {
+                if (sistemaA->PlanetasSistema[i].Luas[j].RaioLua > maiorRaioLuaA) {//Passa por todas as luas do sistema A
+                    maiorRaioLuaA = sistemaA->PlanetasSistema[i].Luas[j].RaioLua;
+                }
+            }
+        }
+
+        for (int i = 0; i < sistemaB->QuantPlanetas; i++) {
+            for (int j = 0; j < sistemaB->PlanetasSistema[i].QuantLuas; j++) {
+                if (sistemaB->PlanetasSistema[i].Luas[j].RaioLua > maiorRaioLuaB) {//Passsa por todas as luas do sistema B
+                    maiorRaioLuaB = sistemaB->PlanetasSistema[i].Luas[j].RaioLua;
+                }
+            }
+        }
+
+         //Pega a maior lua do sistema A e compara com a maior lua do sistema B e retorna o resultado
+        if (maiorRaioLuaA > maiorRaioLuaB) {
+            return 1;
+        } else if (maiorRaioLuaA < maiorRaioLuaB) {
+            return 0;
+        }
+    }
+
+    if (sistemaA->TempoDescoberta > sistemaB->TempoDescoberta) {//Menor tempo de descobrimento
+        return 1;
+    } else if (sistemaA->TempoDescoberta < sistemaB->TempoDescoberta) {
+        return 0;
+    }
+}
